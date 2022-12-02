44049e4070e095dfaf11919a45506eb5789711fc
ItsGifNotJif <115978042+ItsGifNotJif@users.noreply.github.com>
2022-12-02 19:09:58 +0200

    Add files via upload

---
+++ part 1/main.js
@@ -0,0 +1,68 @@
+
+
+
+console.log('welcome to 30 Days of Javascript');
+
+
+
+console.log(typeof 'Asabeneh');
+console.log(typeof 5);
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+let firstName = 'Asabeneh' 
+let lastName = 'Yetayeh' 
+let country = 'Finland' 
+let city = 'Helsinki' 
+let age = 100 
+let isMarried = true
+
+console.log(firstName, lastName, country, city, age, isMarried);
+
+let age1 = 100;
+const gravity = 9.81;
+const boilingPoint = 100;
+const PI = 3.14;
+
+console.log(age1, gravity, boilingPoint, PI); // comments cant make code readable
+
+// Welcome to 30 days of Javascript
+
+/* comments can make code readable,
+easy to reuse and informative */ 
+
+const string = "string";
+const boolean = true;
+const undefined1 = '';
+const null1 = null;
+
+console.log(typeof string);
+console.log(typeof boolean);
+console.log(typeof undefined);
+console.log(typeof null);
+
+const var11 = 5;
+const var22 = 1;
+const var33 = 55;
+const var44 = 10;
+
+{
+    const name = 'Dorian';
+const surname = 'Ashgray';
+const status = 'single';
+const countryMain = 'Lithuania';
+const ageReal = 27;
+}
+
+const name = 'Dorian', surname = 'Ashgray', ageReal = 27, status = 'single', countryMain = 'Lithuania';
+
+const yourAge = 25;
+const myAge = 27;
+
+console.log("I am " + yourAge + " years old" );
+console.log("You are " + myAge + " years old");
+
+
+
