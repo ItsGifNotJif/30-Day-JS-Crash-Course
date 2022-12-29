098ba5e4419fe30f4ecde8fa040c0a47720a5d87
It's Gif Not Jif <jdavindsale@gmail.com>
2022-12-29 17:05:39 +0200

    part 8 update

--- part 8/main.js
+++ part 8/main.js
@@ -323,9 +323,77 @@ let b = 10; // Global scope object, accessible anywhere within the file;
     console.log(dog.bark())
 
     dog.breed = "Hellhound",
-    dog.getDogInfo = function(){
-        let statement = `${this.name}, ${this.legs}, ${this.breed}, ${this.color}`
-        return statement;
-    }
+        dog.getDogInfo = function () {
+            let statement = `${this.name}, ${this.legs}, ${this.breed}, ${this.color}`
+            return statement;
+        }
     console.log(dog.getDogInfo())
+}
+
+{
+    const users = {
+        Alex: {
+            email: 'alex@alex.com',
+            skills: ['HTML', 'CSS', 'JavaScript'],
+            age: 20,
+            isLoggedIn: false,
+            points: 30
+        },
+        Asab: {
+            email: 'asab@asab.com',
+            skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
+            age: 25,
+            isLoggedIn: false,
+            points: 50
+        },
+        Brook: {
+            email: 'daniel@daniel.com',
+            skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+            age: 30,
+            isLoggedIn: true,
+            points: 50
+        },
+        Daniel: {
+            email: 'daniel@alex.com',
+            skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+            age: 20,
+            isLoggedIn: false,
+            points: 40
+        },
+        John: {
+            email: 'john@john.com',
+            skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+            age: 20,
+            isLoggedIn: true,
+            points: 50
+        },
+        Thomas: {
+            email: 'thomas@thomas.com',
+            skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+            age: 20,
+            isLoggedIn: false,
+            points: 40
+        },
+        Paul: {
+            email: 'paul@paul.com',
+            skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
+            age: 20,
+            isLoggedIn: false,
+            points: 40
+        }
+    }
+
+
+    let manySkills = 0;
+    let userMaxSkills;
+
+    for (const user in users) {
+        if (users[user].skills.length > manySkills) {
+            manySkills = users[user].skills.length;
+            userMaxSkills = user;
+        }
+    }
+    console.log(`user with most skills is ${userMaxSkills}`)
+
+
 }
